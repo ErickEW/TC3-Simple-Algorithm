f9e46b75b651204d2443152ce74d351aa870c874
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-09-28 15:12:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Simple_algorithm
+{
+    class Program
+    {
+   static void Main(string[] args)
+        {
+            int Num;
+
+            Console.WriteLine("Inserta un numero");
+
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.Write(Num);
+
+            for (int i = 1; i <= Num; i ++)
+            {
+              System.Console.Write(" i, ");
+            }
+
+            Console.WriteLine(Num);
+
+        }
+    }
+}
+//To do : Que se muestren los numeros entre los 2 numeros que insertamos
